e21fa135851b49b0d9c4dcd676e3325303024e56
azan788 <72837805+azan788@users.noreply.github.com>
2021-01-26 11:22:08 -0500

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,26 @@
+class Block {
+  constructor(x, y, width, height){
+    var options = {
+      'restitution':0.5
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.Visibility = 225;
+    this.width = width;
+    this.height = height;
+    World.add(world, this.body);
+  }
+  display(){
+    if(this.body.speed < 3){
+        var pos = this.body.position;
+        push();
+        rectMode(CENTER);
+        rect(pos.x,pos.y, this.width, this.height);
+        pop();
+    } else {
+        World.remove(world, this.body);
+        push();
+        this.Visibility = this.Visibility - 5;
+        pop();
+    }
+  }
+}
